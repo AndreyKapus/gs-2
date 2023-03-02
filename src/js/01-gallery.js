7aa341e6287324282d7d5d3362cbf08ec2d10979
AndreyKapus <kapustnikov@ukr.net>
2023-03-02 13:55:16 +0200

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,16 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const galleryEl = document.querySelector('.gallery');
+console.log(galleryEl)
 
-console.log(galleryItems);
+const renderGellery = () => {
+    const pictures = galleryItems.map(({preview, original, description}) => {
+     return `<div>
+     <img src=${preview} alt=${description} class="gallery__image"/>
+     </div>`
+    }).join('')
+    return pictures;
+};
+const galleryListMarkup = renderGellery(galleryItems)
+galleryEl.insertAdjacentHTML('beforeend', galleryListMarkup)
+
+// console.log(galleryItems);
