338fb91bf0776738d79fa9b54cd86c4c11a8ac3f
AndreyKapus <kapustnikov@ukr.net>
2023-03-03 13:57:56 +0200

    modal

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,11 +1,11 @@
 import { galleryItems } from './gallery-items.js';
 const galleryEl = document.querySelector('.gallery');
-console.log(galleryEl)
+
 
 const renderGellery = () => {
     const pictures = galleryItems.map(({preview, original, description}) => {
-     return `<div>
-     <img src=${preview} alt=${description} class="gallery__image"/>
+     return `<div class="gallery__item">
+     <img src=${preview} alt=${description} data-source=${original} class="gallery__image"/>
      </div>`
     }).join('')
     return pictures;
@@ -14,3 +14,24 @@ const galleryListMarkup = renderGellery(galleryItems)
 galleryEl.insertAdjacentHTML('beforeend', galleryListMarkup)
 
 // console.log(galleryItems);
+
+const onImageClick = (e) => {
+    if(!e.target.classList.contains('gallery__image')) {
+        return
+    }
+ 
+    const image = e.target.dataset.source
+
+    const instance = basicLightbox.create(`
+    <img src=${image} width="800" height="600">
+`)
+    instance.show()
+};
+
+
+
+const onGalleryItemClick = galleryEl.addEventListener('click', onImageClick)
+
+
+
+
