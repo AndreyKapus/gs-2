61920fe8afd04335f2060e08868d15d752d9cbd1
AndreyKapus <kapustnikov@ukr.net>
2023-03-04 12:48:42 +0200

    second

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const galleryEl = document.querySelector('.gallery');
+
+
+const renderGellery = () => {
+    const pictures = galleryItems.map(({preview, original, description}) => {
+     return `<a class="gallery__item" href=${original}>
+     <img class="gallery__image" src=${preview} alt=${description} />
+   </a>`
+    }).join('')
+    return pictures;
+};
+
+const galleryItemsMarkup = renderGellery(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', galleryItemsMarkup)
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
+});
+
+
 
 console.log(galleryItems);
